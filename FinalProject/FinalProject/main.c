94d37b4ac1b874231dbbd2f1f625f7093690c89c
62fe50 <merrickslane@gmail.com>
2021-11-19 13:27:14 -0800

    lab10 and lab11

---
+++ FinalProject/FinalProject/main.c
@@ -0,0 +1,18 @@
+/*
+ * FinalProject.c
+ *
+ * Created: 11/19/2021 12:50:59 PM
+ * Author : Merrick
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
